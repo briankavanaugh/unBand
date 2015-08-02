234ed41777f150e7b4a13acdfed610e6ce6f3d39
bkavanaugh <brian@briankavanaugh.com>
2015-08-02 07:11:28 -0500

    Remove unused properties

--- src/unBand.Cloud/Exporters/EventExporters/Helpers/GolfEventHoleSequenceDumper.cs
+++ src/unBand.Cloud/Exporters/EventExporters/Helpers/GolfEventHoleSequenceDumper.cs
@@ -13,14 +13,7 @@ namespace unBand.Cloud.Exporters.EventExporters.Helpers {
 				{"User Score", item.UserScore},
 				{"Hole Shot Overlay Image Url", item.HoleShotOverlayImageUrl},
 				{"Step Count", item.StepCount},
-				{"Distance Walked (cm)", item.DistanceWalkedInCm},
-				{"Heart Rate Zone - Aerobic", item.HeartRate.Zones.Aerobic},
-				{"Heart Rate Zone - Anaerobic", item.HeartRate.Zones.Anaerobic},
-				{"Heart Rate Zone - Fitness Zone", item.HeartRate.Zones.FitnessZone},
-				{"Heart Rate Zone - Healthy Heart", item.HeartRate.Zones.HealthyHeart},
-				{"Heart Rate Zone - Over", item.HeartRate.Zones.Over},
-				{"Heart Rate Zone - Red Line", item.HeartRate.Zones.RedLine},
-				{"Heart Rate Zone - Under", item.HeartRate.Zones.Under},
+				{"Distance Walked (cm)", item.DistanceWalkedInCm}
 			};
 		}
 	}
